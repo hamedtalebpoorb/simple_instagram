bfcded19f3f567e6f1e94872ac5a40ee74e9110e
hamedtalebpoorb <hamedtalebpoorb@gmail.com>
2017-11-14 15:20:02 +0330

    first try for confirmation with mail

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   # validates :user_name , length:{minimum:4 ,maximum:16}
   has_many :posts, dependent: :destroy
   has_many :comments,dependent: :destroy
-  validates :user_name , length:{maximum:16}	
+  validates :user_name , length:{maximum:16}
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         :confirmable
 end
