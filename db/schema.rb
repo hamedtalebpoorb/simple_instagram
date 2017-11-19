d656a73e44bdbf2aded5eb92a173560565fe4956
hamedtalebpoorb <hamedtalebpoorb@gmail.com>
2017-11-19 21:16:07 +0330

    some css added for devise/sessions/new and registration/new

--- db/schema.rb
+++ db/schema.rb
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20171112114833) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "user_name"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["user_name"], name: "index_users_on_user_name", unique: true
