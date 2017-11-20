e47c4659bffa3bc45cdd4e010884a9e4b19f2104
hamedtalebpoorb <hamedtalebpoorb@gmail.com>
2017-11-21 00:43:26 +0330

    comments constraint improved

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,43 +2,29 @@ class CommentsController < ApplicationController
 	before_action :set_post
 
 	def create
+
 		@comment = @post.comments.build(comment_params)
 		@comment.user_id = current_user.id
 
-		# collection.each do |variable|
-   # 		code
-		# end
-
 		Comment.all.each do |comment|
-
-			# comment.update(comments_count: 0)
-			# comments_count = comment.comments_count.to_i
    		if @comment.user_id == comment.user_id && @comment.post_id == comment.post_id
-				p "yyyyyyyyyyyyyyyyyyyyyyyyyyyyyy"
    			@comment.comments_count = @comment.comments_count + 1
    		end
 		end
 
-
-		if (@comment.save && @comment.comments_count <= 4)
-			p "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"
-			p @comment.comments_count
-				# p "pay attention:"
-				# p @comment.comments_count
-				# p comment.user_id
-				#end
-				# p @comment
-				# p @comment.user_id
-				# p @comment.post_id
-				# p @comment.comments_count
-			respond_to do |format|
-				#format.html { redirect_to root_path }
-				format.js
-			end
+		if @comment.comments_count <= 4
+			if @comment.save #innier if
+				respond_to do |format|
+					#format.html { redirect_to root_path }
+					format.js
+				end
+			end #innier end
 		else
-			# flash[:alert] = "not commented!"
+			# flash[:notice] = "not commented!"
 			render root_path
+			# p "hiiiiiiiiiiiiiiiiiiiiiiiiii"
 		end
+
 	end
 
 	def destroy
