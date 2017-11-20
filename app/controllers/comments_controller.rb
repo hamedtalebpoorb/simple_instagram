450c2358d705d8fb987d7c3c81b1a6cfe91a0f2d
hamedtalebpoorb <hamedtalebpoorb@gmail.com>
2017-11-20 17:44:54 +0330

    comments constraint per user created

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,30 +10,33 @@ class CommentsController < ApplicationController
 		# end
 
 		Comment.all.each do |comment|
-			comment.update(comments_count: 0)
+
+			# comment.update(comments_count: 0)
 			# comments_count = comment.comments_count.to_i
    		if @comment.user_id == comment.user_id && @comment.post_id == comment.post_id
-				# p "xyzzzzzzzzzzzzzzzzzzzzzzzzzz"
-   			comment.comments_count = comment.comments_count + 1
+				p "yyyyyyyyyyyyyyyyyyyyyyyyyyyyyy"
+   			@comment.comments_count = @comment.comments_count + 1
    		end
 		end
 
 
-		if (@comment.save)
-			p "pay attention:"
+		if (@comment.save && @comment.comments_count <= 4)
+			p "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"
 			p @comment.comments_count
+				# p "pay attention:"
+				# p @comment.comments_count
 				# p comment.user_id
-			#end
-			# p @comment
-			# p @comment.user_id
-			# p @comment.post_id
-			# p @comment.comments_count
+				#end
+				# p @comment
+				# p @comment.user_id
+				# p @comment.post_id
+				# p @comment.comments_count
 			respond_to do |format|
 				#format.html { redirect_to root_path }
 				format.js
 			end
 		else
-			flash[:alert] = "not commented!"
+			# flash[:alert] = "not commented!"
 			render root_path
 		end
 	end
